51ee11fb0ddfd483de8d9de1a34329fc6e68acfc
Anthony Lucci <tonylucci@gmail.com>
2020-03-05 00:17:19 +0000

    Automatically backed up by Learn

--- hello_world.rb
+++ hello_world.rb
@@ -1,1 +1,5 @@
 puts "Hello World!"
+ruby hello_world.#!/usr/bin/env ruby -wKU
+$ ruby hello_world.#!/usr/bin/env ruby -wKU
+ruby hello_world.#!/usr/bin/env ruby -wKU
+$ ruby hello_world.#!/usr/bin/env ruby -wKU
